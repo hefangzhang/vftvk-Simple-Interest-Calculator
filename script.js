11be19b9912fab17bab77ce083fd06c7ce4a2610
hefang <hefang_zhang@163.com>
2021-01-26 16:12:59 +0000

    update

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-function compute()
-{
-    principal = document.getElementById("principal").value;
-    rate=document.getElementById("rate").value;
-    years=document.getElementById("years").value;
-    interest=principal*years*rate/100;
-    
-    
+function compute() {
+    var principal = document.getElementById("principal").value;
+    // check if the amount is positive, otherwise throw an error and stop.
+    if (principal < 1 || principal == "") {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var date = new Date();
+    var thisYear = date.getFullYear();
+    // need to convert years (string) to numbers
+    var futureYear = thisYear + Number(years);
+    var result = document.getElementById("result");
+    output = "If you deposit <mark>" + principal + "</mark>,<br>"
+            + "at an interest rate of <mark>" + rate + "</mark>%.<br>"
+            + "You will receive an amount of <mark>" + interest + "</mark>,<br>"
+            + "in the year <mark>" + futureYear + "</mark>";
+    result.innerHTML = output;
 }
         
